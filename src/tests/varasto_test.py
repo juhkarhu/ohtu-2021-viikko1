f5c9787de14f794ea59fc25b6395bf696d2e4f0c
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-11-01 13:50:43 +0200

    broke tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -11,7 +11,7 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_varaston_str_toimii_tyhjalla_varastolla(self):
-        self.assertEqual(str(self.varasto), 'saldo = 0, vielä tilaa 10')
+        elf.assertEqual(str(self.varasto), 'saldo = 0, vielä tilaa 10')
 
     def test_konstruktori_toimii_negatiisivella_tilavuudella(self):
         self.warehouse = Varasto(-2)
